4f5e56fe9b54b63ef886b67a1e5757ffef0dd100
G SEKHAR REDDY <sekhar.reddy.cse.2023@nist.edu>
2025-07-21 20:38:30 +0530

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/day4/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/day4/ExampleInstrumentedTest.java
@@ -1,7 +1,7 @@
 package com.example.day4;
 
 import android.content.Context;
- 
+import android.widget.Toast;   
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
